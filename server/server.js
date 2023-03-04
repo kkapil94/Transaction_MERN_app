00d34d47549e873774f7bb1387429b33a9ede049
kapil <khatrikapil987@gmail.com>
2023-03-04 22:28:28 +0530

    deploy commit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import passportConfig from "./config/passport.js"
 
 await connect();
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(bodyParser.json())
